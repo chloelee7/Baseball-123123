05ae28a1fec3fac39f25c3daed91b87b96bc9901
chloelee7 <chloe.lee7@emcore.kr>
2026-05-28 13:52:44 +0900

    [feature] 2

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -26,4 +26,11 @@ class GameTest {
         });
     }
 
+    @Test
+    void throwExceptionWhenInputLengthIsUnmatched() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            game.guess("12");
+        });
+    }
+
 }
