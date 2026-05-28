014a6e4264ed3e4c0f455a352eaea676ad6bd365
chloelee7 <chloe.lee7@emcore.kr>
2026-05-28 13:48:33 +0900

    [feature] 1

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:6.0.0"))
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.24.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,5 +1,23 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class GameTest {
+
+    @Test
+    void createGame() {
+        Game game = new Game();
+        assertNotNull(game);
+    }
+
+    @Test
+    void throwExceptionWhenInputIsNull() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Game game = new Game();
+            game.guess(null);
+        });
+    }
 
 }
